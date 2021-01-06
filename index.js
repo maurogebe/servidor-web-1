74c1fb9defd560d7897bc27aee5cb1cb6087d815
maurogebe <maurogebe@MYS.localdomain>
2021-01-05 21:51:14 -0500

    Terminada las rutas con los condicionales

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
-const { doesNotMatch } = require("assert");
 let http = require("http");
+let fs = require("fs")
 let morgan = require("morgan")
 
 let logger = morgan('tiny')
 
+// const routes = ['/', '/about', '/projects', '/contact', '/404']
+
+function fsFn(route, response) {
+    fs.readFile(route, (error, content) => {
+        if(!error) {
+            response.write(content)
+            response.end()
+        }
+    })
+}
+
 http.createServer((request, response) => {
+    response.setHeader("Content-Type", "text/html; charset=utf-8")
     logger(request, response, function (err) {
         if(err) return doesNotMatch(err)
 
         if(request.url === "/") {
-            response.setHeader("Content-Type", "text/html; charset=utf-8")
-            response.write("<h1>Pagina de Home</h1>")
-            response.end()
-        } else if(request.url === "/contacto") {
-            response.setHeader("Content-Type", "text/html; charset=utf-8")
-            response.write("<h1>Pagina de contacto</h1>")
-            response.end()
+            fsFn('./index.html', response)
+        } else if(request.url === "/about") {
+            fsFn('./about.html', response)
+        } else if(request.url === "/projects") {
+            fsFn('./projects.html', response)
+        } else if(request.url === "/contact") {
+            fsFn('./contact.html', response)
         } else {
-            response.setHeader("Content-Type", "text/html; charset=utf-8")
-            response.write("<h1>404</h1>")
-            response.end()
+            fsFn('./404.html', response)
         }
+
+        // switch (true) {
+        //     case request.url === "/":
+        //         // fs('./index.html')
+        //         fs.readFile('./index.html', (error, content) => {
+        //             if(!error) {
+        //                 response.write(content)
+        //                 response.end()
+        //             }
+        //         })
+        //         break
+        //     case request.url === "/about":
+        //         fs('./about.html')
+        //         break
+        //     case request.url === "/projects":
+        //         fs('./projects.html')
+        //         break
+        //     case request.url === "/contact":
+        //         fs('./contact.html')
+        //         break
+        //     default:
+        //         fs('./404.html')
+        //         break
+        // }
     })
 }).listen(8080);
