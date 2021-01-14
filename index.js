c2863373d5905c9745083c1f05dc633ef2c04cea
Mauricio Guerra <maurogebe.96@gmail.com>
2021-01-13 19:20:34 -0500

    Agregado el form

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
 let http = require("http");
 let fs = require("fs")
 let morgan = require("morgan")
+let path = require("path")
+let mime = require("mime")
 
 let logger = morgan('tiny')
 
-function fsFn(path, response) {
-    fs.readFile(path, (error, content) => {
+const fsFn = (url, response) => {
+    const urlF = __dirname + url
+    fs.readFile(urlF, (error, content) => {
         if(!error) {
-            response.write(content)
-            response.end()
+            response.setHeader("Content-Type", mime.getType(urlF))
+            response.end(content)
+        } else {
+            response.statusCode = 404
+            response.writeHead(404)
+            response.end('<h1>405</h1>')
         }
     })
 }
 
 http.createServer((request, response) => {
-    response.setHeader("Content-Type", "text/html; charset=utf-8")
     logger(request, response, function (err) {
         if(err) return doesNotMatch(err)
 
-        switch (request.url) {
-            case "/": 
-                fsFn('./index.html', response)
-                break
-            case "/about": 
-                fsFn('./about.html', response)
-                break
-            case "/projects": 
-                fsFn('./projects.html', response)
-                break
-            case "/contact": 
-                fsFn('./contact.html', response)
-                break
-            case "/favicon.ico": 
-            response.setHeader("Content-Type", "image/x-icon; charset=utf-8")
-                fsFn('./favicon.ico', response)
-                break
-            default:
-                fsFn('./404.html', response)
-        }
+        if(request.method === "GET") {
+            switch (request.url) {
+                case "/": 
+                    fsFn('/index.html', response)
+                    break
+                case "/about": 
+                    fsFn('/about.html', response)
+                    break
+                case "/projects": 
+                    fsFn('/projects.html', response)
+                    break
+                case "/contact": 
+                    fsFn('/contact.html', response)
+                    break
+                case "/favicon.ico": 
+                    fsFn('/favicon.ico', response)
+                    break
+                default:
+                    fsFn(request.url, response)
+            }    
+        } else if(request.method === "POST") {
+            let users = []
 
+            // Cuando se estan recibiendo los datos
+            request.on('users', chunk => {
+                users.push(chunk)
+            })
+
+            // Cuando se terminan de procesar los datos
+            request.on('end', () => {
+                console.log(users.toString)
+                console.log("The End")
+            })
+        }
+        
     })
 }).listen(8080);
